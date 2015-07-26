997c9fe0dec57a0fc7b54036b159230de819e021
Juan Jesus Gomez Noya <burus_86@hotmail.com>
2015-07-26 20:22:06 +0200

    Copiado el código de las transparencias del módulo 7

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+// Página de entrada (home page)
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quiz' });
+});
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+// GET author page
+router.get('/author', function(req, res, next) {
+  res.render('author', { });
+});
+
+module.exports = router;
